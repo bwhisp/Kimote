6b34baa8dc3c0d3f1de1f50cbd4a8a7d3087dcc6
bwhisp <bwh.anass@gmail.com>
2015-02-15 22:37:09 +0100

    Connexion

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -62,16 +62,40 @@ appCtrl.controller('PicsCtrl', function($scope) {
 
 appCtrl.controller('SettingsCtrl', function($scope) {
 	$scope.menu = 'settings';
+	$scope.errCon = false;
 
 	$scope.IPMODEL = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
 
-	$scope.login = function() {
+	$scope.ip = "127.0.0.1";
+	$scope.port = 8080;
+	$scope.user = "kodi";
+	$scope.pass = "admin";
+
+	var connectSuccess = function (data, status) {
+		$scope.errCon = false;
+		alert(data.results[0]);
+		//deferred.resolve();
+	}
+
+	var connectError = function (data, status) {
+		$scope.errCon = true;
+		deferred.reject(data);
+		alert("Fail");
+	}
+
+	$scope.login = function($http, $q) {
+		var deferred = $q.defer();
     	ip = $scope.ip;
-	    port = $scope.port.toString();
+	    port = $scope.port;
     	username = $scope.user;
     	password = $scope.pass;
 	
-		base_url = 'http://' + username + ':' + password + '@' + ip + ':' + port;
+		//$http.get(base_url).success(connectSuccess).error(connectError)
     	//$location.path(base_url);
+		base_url = 'http://' + username + ':' + password + '@' + ip + ':' + port; // base_url
+    	test_url = '/jsonrpc?request={ "jsonrpc": "2.0", "method": "JSONRPC.Ping", "id": 1 }';
+		$http.jsonp(base_url+test_url).success(connectSuccess(deferred)).error(connectError(deferred));
 	}
+
+
 });
