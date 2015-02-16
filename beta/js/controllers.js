a5804d95617119d9ef4a2571ea3aa2e5d44c6dcb
aplanchamp <aplanchamp@gmail.com>
2015-02-16 11:47:34 +0100

    changement beta test

--- beta/js/controllers.js
+++ beta/js/controllers.js
@@ -1,11 +1,11 @@
 var appCtrl = angular.module('appCtrl', []);
 
 appCtrl.controller('RemoteCtrl', function($scope,$http) {
-	$scope.menu = 'remote';
 
 	$scope.request = function request(input) {
 		method = 'Input.';
-	    
+		params = '';
+		
 	    if (input === 'left') {
 	        method = method + 'Left';
 	    }
@@ -29,57 +29,37 @@ appCtrl.controller('RemoteCtrl', function($scope,$http) {
 	    }
 	    else if (input === 'shutdown') {
 	        method = 'Application.';
-	        method = method + 'Quit'; //OnQuit = quitter Kodi, System.Shutdown = éteindre le système
+	        method = method + 'Quit';
+
 	    }
-	    else if(input == 'setmute') {
+		else if (input === 'connect') {
+			method = '';
+		}
+		else if(input === 'mute') {
 	    	method = 'Application.';
 	    	method = method + 'SetMute'; 
+			params = ',"params":{"mute":true}';
 	    }
-	    else if(input == 'setvolume'){
+		else if(input === 'volumeUp') {
+	    	method = 'Application.';
+	    	method = method + 'SetVolume'; 
+			params = ',"params":{"volume":' + '-' + 100 + '}';
+	    }		
+		
+	    else if(input === 'SetVolume'){
 	    	method = 'Application';
-	    	method = method + 'SetVolume';
+	    	method = method + 'SetVolume' + '"params":{"volume":' + 100 + '},"permission":"ControlPlayback"';
 	    }
 
 	    sendRequest($http, method);
 	}
 
 	function sendRequest($http, method) {
-		param_url = '/jsonrpc?request={"jsonrpc": "2.0", "method": "' + method + '"}';
-    	complete_url = window.base_url + param_url;
-
-		$http.jsonp(complete_url);
-
-		/*.error(function() {
-			alert('Impossible de se connecter');
-		});*/
-	}
-});
-
-
-appCtrl.controller('MoviesCtrl', function($scope) {
-	$scope.menu = 'movies';
-});
-
-appCtrl.controller('MusicCtrl', function($scope) {
-	$scope.menu = 'music';
-});
-
-appCtrl.controller('PicsCtrl', function($scope) {
-	$scope.menu = 'pics';
-});
-
-appCtrl.controller('SettingsCtrl', function($scope) {
-	$scope.menu = 'settings';
-
-	$scope.IPMODEL = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
-
-	$scope.login = function() {
-    	ip = $scope.ip;
-	    port = $scope.port.toString();
-    	username = $scope.user;
-    	password = $scope.pass;
 	
-		base_url = 'http://' + username + ':' + password + '@' + ip + ':' + port;
-    	//$location.path(base_url);
+		param_url = 'http://kodi:coucou@192.168.22.30:8080/jsonrpc?request={"jsonrpc": "2.0", "method": "' + method + '" '+ params +' }';
+
+		$http.jsonp(param_url);
+
 	}
+	
 });
