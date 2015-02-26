16a302176d3e29b7d3d10840bab32e7eaa816cf9
aplanchamp <aplanchamp@gmail.com>
2015-02-26 18:40:15 +0100

    design et barre volume beta

--- beta/js/app.js
+++ beta/js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('deviceready', function (){
 
 }, false);
 
-var app = angular.module('app', ['ngRoute', 'appCtrl']);
+var app = angular.module('app', ['ngRoute', 'ngMaterial', 'ngAnimate', 'appCtrl']);
 
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
@@ -30,3 +30,52 @@ app.config(['$routeProvider', function ($routeProvider) {
 			redirectTo: '/remote'
 		});
 }]);
+
+app.factory('Logger', function($http) {
+	var logger = {};
+
+	var connected = false;
+	var errCon = false;
+	var bouton = "Connecter";
+
+	logger.logout = function () {
+		window.base_url = "";
+		connected = false;
+		errCon = false;
+		bouton = "Connecter";
+		alert("plop");
+	};
+
+	logger.login = function (username, password, ip, port) {
+		window.base_url = 'http://' + username + ':' + password + '@' + ip + ':' + port;
+		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"JSONRPC.Ping"}&callback=JSON_CALLBACK';
+
+		$http.jsonp(window.base_url+ping_url)
+			.success(function(data, status){
+				connected = true;
+				errCon = false;
+				bouton = "Déconnecter";
+				window.location = "#/remote";
+			})
+			.error(function(data, status){
+				connected = false;
+				errCon = true;
+				bouton = "Connecter";
+				alert("Error"+status);
+			});
+	};
+
+	logger.getConn = function () {
+		return connected;
+	};
+
+	logger.getErr = function () {
+		return errCon;
+	};
+
+	logger.getBouton = function () {
+		return bouton;
+	};
+
+	return logger;
+});
