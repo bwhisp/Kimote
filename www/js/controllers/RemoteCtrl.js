ebebc4509c79558ea37d7624fb428fa2346513a4
votre_pseudo <moi@email.com>
2015-03-18 11:16:15 +0100

    barre runtime

--- www/js/controllers/RemoteCtrl.js
+++ www/js/controllers/RemoteCtrl.js
@@ -1,138 +1,154 @@
-//var vol = 50;
+	//var vol = 50;
 
-app.controller('RemoteCtrl', function($scope,$http, Sounder, Manager) {
-	
+	app.controller('RemoteCtrl', function($scope,$http, Sounder, Manager, Runtime) {
+		
+		$scope.runtime;
+		$scope.getRuntime = function () {
+		$scope.runtime=Runtime.GetRuntime();
+		};
 
-	$scope.muted = Sounder.getMuted();
-	$scope.volume = Sounder.getVolume();
-	$scope.sound = Sounder.getVolume();	
-	
-	$scope.paused=Manager.getPaused();
-	$scope.played=Manager.getPlayed();
-	
-	$scope.setVol = function () {
-		Sounder.SetVol($scope.sound);
-		$scope.sound = Sounder.getVolume();
-	};
+		setInterval($scope.getRuntime,500);
+		$scope.setRuntime = function () {
+			Runtime.SetRuntime($scope.runtime);	
+		};
+		
 
-	$scope.requestInput = function requestInput(input) {
-		method = 'Input.';
+		$scope.muted = Sounder.getMuted();
+		$scope.volume = Sounder.getVolume();
+		$scope.sound = Sounder.getVolume();	
+		
+		$scope.paused=Manager.getPaused();
+		$scope.played=Manager.getPlayed();
+		
+		$scope.setVol = function () {
+			Sounder.SetVol($scope.sound);
+			$scope.sound = Sounder.getVolume();
+		};
 
-		if (input === 'left') {
-			method = method + 'Left';
-		}
-		else if (input === 'right') {
-			method = method + 'Right';
-		}
-		else if (input === 'up') {
-			method = method + 'Up';
-		}
-		else if (input === 'down') {
-			method = method + 'Down';
-		}
-		else if (input === 'select') {
-			method = method + 'Select';
-		}
-		else if (input === 'home') {
-			method = method + 'Home';
-		}
-		else if (input === 'back') {
-			method = method + 'Back';
-		}
-		else if (input === 'play') {
+		$scope.requestInput = function requestInput(input) {
+			method = 'Input.';
+
+			if (input === 'left') {
+				method = method + 'Left';
+				params = '{}';
+			}
+			else if (input === 'right') {
+				method = method + 'Right';
+				params = '{}';
+			}
+			else if (input === 'up') {
+				method = method + 'Up';
+				params = '{}';
+			}
+			else if (input === 'down') {
+				method = method + 'Down';
+				params = '{}';
+			}
+			else if (input === 'select') {
+				method = method + 'Select';
+				params = '{}';
+			}
+			else if (input === 'home') {
+				method = method + 'Home';
+				params = '{}';
+			}
+			else if (input === 'back') {
+				method = method + 'Back';
+				params = '{}';
+			}
+			else if (input === 'play') {
+				
+				Manager.SetPlay();
+				$scope.played=Manager.getPlayed();
+				$scope.paused=Manager.getPaused();
+				params = '{}';
+				
+			}
+			else if (input === 'pause') {
+				Manager.SetPause();
+				$scope.played=Manager.getPlayed();
+				$scope.paused=Manager.getPaused();
+				params = '{}';
+			}
+			else if (input === 'stop') {
+				method = method + 'ExecuteAction';
+				params = '{"action":"stop"}';
+			}
+			else if (input === 'next') {
+				method = method + 'ExecuteAction';
+				params = '{"action":"skipnext"}';
+			}
+			else if (input === 'previous') {
+				method = method + 'ExecuteAction';
+				params = '{"action":"skipprevious"}';
+			}
+			else if (input === 'fastforward') {
+				method = method + 'ExecuteAction';
+				params = '{"action":"fastforward"}';
+			}
+			else if (input === 'rewind') {
+				method = method + 'ExecuteAction';
+				params = '{"action":"rewind"}';
+			}
+
+			sendRequestWithParams($http, method, params);
+		};
+
+		$scope.requestApplication = function requestApplication(input) {
+			method = 'Application.';
+
+			if (input === 'shutdown') {
+				method = method + 'Quit';
+				params = '{}';
+			}
+			else if (input === 'mute') {
+				Sounder.SetMute();
+				$scope.muted = Sounder.getMuted();	
+			}
+			else if (input === 'unmute') {
+				Sounder.SetUnMute();
+				$scope.muted = Sounder.getMuted();
+			}
+			else if (input === 'volumeUp') {
+				Sounder.VolUp($scope.volume);
+				$scope.volume = Sounder.getVolume();
+			}
+			else if (input === 'volumeDown') {
+				Sounder.VolDown($scope.volume);
+				$scope.volume = Sounder.getVolume();
+			}
+
+			sendRequestWithParams($http, method, params);
+		};
+
+		$scope.requestGUI = function requestGUI(input) {
 			
-			Manager.SetPlay();
-			$scope.played=Manager.getPlayed();
-			$scope.paused=Manager.getPaused();
-			
-		}
-		else if (input === 'pause') {
-			Manager.SetPause();
-			$scope.played=Manager.getPlayed();
-			$scope.paused=Manager.getPaused();
-		}
-		
-		sendRequest($http,method);
-		
-		if (input === 'stop') {
-			method = method + 'ExecuteAction';
-			params = '{"action":"stop"}';
-		}
-		else if (input === 'next') {
-			method = method + 'ExecuteAction';
-			params = '{"action":"skipnext"}';
-		}
-		else if (input === 'previous') {
-			method = method + 'ExecuteAction';
-			params = '{"action":"skipprevious"}';
-		}
-		else if (input === 'fastforward') {
-			method = method + 'ExecuteAction';
-			params = '{"action":"fastforward"}';
-		}
-		else if (input === 'rewind') {
-			method = method + 'ExecuteAction';
-			params = '{"action":"rewind"}';
+			method = 'GUI.';
+
+			if (input === 'fullscreen') {
+				method = method + 'SetFullscreen';
+				params = '{"fullscreen":true}';
+			}
+
+			sendRequestWithParams($http, method, params);
+		};
+
+		function sendRequest($http, method) {
+			param_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"' + method + '", "id": 1}';
+			complete_url = window.base_url + param_url;
+
+			$http.jsonp(complete_url, {params: {callback: 'JSON_CALLBACK', format: 'json'}}).error(function() {
+				alert("Vous n'êtes pas connecté");
+			});
 		}
 
-		sendRequestWithParams($http, method, params);
-	};
+		function sendRequestWithParams($http, method, params) {
+			param_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"' + method + '","params": '+ params +', "id": 1}';
+			complete_url = window.base_url + param_url;
 
-	$scope.requestApplication = function requestApplication(input) {
-		method = 'Application.';
-
-		if (input === 'shutdown') {
-			method = method + 'Quit';
-			params = '{}';
+			$http.jsonp(complete_url, {params: {callback: 'JSON_CALLBACK', format: 'json'}})
+			.error(function() {
+				alert("Vous n'êtes pas connecté");
+			});
 		}
-		else if (input === 'mute') {
-			Sounder.SetMute();
-			$scope.muted = Sounder.getMuted();	
-		}
-		else if (input === 'unmute') {
-			Sounder.SetUnMute();
-			$scope.muted = Sounder.getMuted();
-		}
-		else if (input === 'volumeUp') {
-			Sounder.VolUp($scope.volume);
-			$scope.volume = Sounder.getVolume();
-		}
-		else if (input === 'volumeDown') {
-			Sounder.VolDown($scope.volume);
-			$scope.volume = Sounder.getVolume();
-		}
-
-		sendRequestWithParams($http, method, params);
-	};
-
-	$scope.requestGUI = function requestGUI(input) {
-		
-		method = 'GUI.';
-
-		if (input === 'fullscreen') {
-			method = method + 'SetFullscreen';
-			params = '{"fullscreen":true}';
-		}
-
-		sendRequestWithParams($http, method, params);
-	};
-
-	function sendRequest($http, method) {
-		param_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"' + method + '", "id": 1}';
-		complete_url = window.base_url + param_url;
-
-		$http.jsonp(complete_url, {params: {callback: 'JSON_CALLBACK', format: 'json'}}).error(function() {
-			alert("Vous n'êtes pas connecté");
-		});
-	}
-
-	function sendRequestWithParams($http, method, params) {
-		param_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"' + method + '","params": '+ params +', "id": 1}';
-		complete_url = window.base_url + param_url;
-
-		$http.jsonp(complete_url, {params: {callback: 'JSON_CALLBACK', format: 'json'}})
-		.error(function() {
-			alert("Vous n'êtes pas connecté");
-		});
-	}
-});
+	});
--- www/js/factories.js
+++ www/js/factories.js
@@ -1,200 +1,275 @@
-app.factory('Logger', function($http) {
-	var logger = {};
+	app.factory('Logger', function($http) {
+		var logger = {};
 
-	var connected = false;
-	var errCon = false;
-	var bouton = "Connecter";
+		var connected = false;
+		var errCon = false;
+		var bouton = "Connecter";
 
-	logger.logout = function () {
-		window.base_url = "";
-		connected = false;
-		errCon = false;
-		bouton = "Connecter";
-		alert("Déconnecté");
-	};
+		logger.logout = function () {
+			window.base_url = "";
+			connected = false;
+			errCon = false;
+			bouton = "Connecter";
+			alert("Déconnecté");
+		};
 
-	logger.login = function (username, password, ip, port) {
-		window.base_url = 'http://' + username + ':' + password + '@' + ip + ':' + port;
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"JSONRPC.Ping"}&callback=JSON_CALLBACK';
+		logger.login = function (username, password, ip, port) {
+			window.base_url = 'http://' + username + ':' + password + '@' + ip + ':' + port;
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"JSONRPC.Ping"}&callback=JSON_CALLBACK';
 
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				connected = true;
-				errCon = false;
-				bouton = "Déconnecter";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-				connected = false;
-				errCon = true;
-				bouton = "Connecter";
-				alert("Error"+status);
-			});
-	};
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					connected = true;
+					errCon = false;
+					bouton = "Déconnecter";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+					connected = false;
+					errCon = true;
+					bouton = "Connecter";
+					alert("Error"+status);
+				});
+		};
 
-	logger.getConn = function () {
-		return connected;
-	};
+		logger.getConn = function () {
+			return connected;
+		};
 
-	logger.getErr = function () {
-		return errCon;
-	};
+		logger.getErr = function () {
+			return errCon;
+		};
 
-	logger.getBouton = function () {
-		return bouton;
-	};
+		logger.getBouton = function () {
+			return bouton;
+		};
 
-	return logger;
-});
+		return logger;
+	});
 
-app.factory('Sounder', function($http) {
-	
-	var sounder = {};
+	app.factory('Sounder', function($http) {
+		var sounder = {};
 
-	var muted = false;
-	var errMute = false;
-	var errUnmute = false;
-	var volume = 50;
+		var muted = false;
+		var errMute = false;
+		var errUnmute = false;
+		var volume = 50;
 
 
-	sounder.SetMute = function () {
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetMute", "params":{"mute":true}}&callback=JSON_CALLBACK';
+		sounder.SetMute = function () {
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetMute", "params":{"mute":true}}&callback=JSON_CALLBACK';
 
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				muted = true;
-				window.location = "#/settings";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-				errMute = true;
-			});
-	};
-	
-	sounder.SetUnMute = function () {
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetMute", "params":{"mute":false}}&callback=JSON_CALLBACK';
-
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				muted = false;
-				window.location = "#/settings";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-				errUnMute = true;
-			});
-	};	
-	
-
-	sounder.VolUp = function (vol) {		
-		if (vol < 100)
-			vol = vol + 1;
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					muted = true;
+					window.location = "#/settings";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+					errMute = true;
+				});
+		};
 		
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetVolume", "params":{"volume":' + vol + '}}&callback=JSON_CALLBACK';
+		sounder.SetUnMute = function () {
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetMute", "params":{"mute":false}}&callback=JSON_CALLBACK';
 
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				volume = vol;
-				window.location = "#/settings";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-			});	
-	}
-	
-	sounder.VolDown = function (vol) {		
-		if (vol < 100)
-			vol = vol - 1;		
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetVolume", "params":{"volume":' + vol + '}}&callback=JSON_CALLBACK';
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					muted = false;
+					window.location = "#/settings";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+					errUnMute = true;
+				});
+		};	
+		
 
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				volume = vol;
-				window.location = "#/settings";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-			});	
-	}	
-	
-	sounder.SetVol = function (sound) {
-	
+		sounder.VolUp = function (vol) {		
+			if (vol < 100)
+				vol = vol + 1;
 			
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetVolume", "params":{"volume":' + sound + '}}&callback=JSON_CALLBACK';
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetVolume", "params":{"volume":' + vol + '}}&callback=JSON_CALLBACK';
 
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				volume = sound;
-				window.location = "#/settings";
-				window.location = "#/remote";				
-			})
-			.error(function(data, status){
-			});	
-	
-	}
-	
-	sounder.getMuted = function () {
-		return muted;
-	};
-	
-	sounder.getVolume = function () {
-			return volume;
-	};
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					volume = vol;
+					window.location = "#/settings";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+				});	
+		}
+		
+		sounder.VolDown = function (vol) {		
+			if (vol < 100)
+				vol = vol - 1;		
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetVolume", "params":{"volume":' + vol + '}}&callback=JSON_CALLBACK';
 
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					volume = vol;
+					window.location = "#/settings";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+				});	
+		}	
+		
+		sounder.SetVol = function (sound) {
+		
+				
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Application.SetVolume", "params":{"volume":' + sound + '}}&callback=JSON_CALLBACK';
 
-
-	return sounder;
-});
-
-app.factory('Manager', function($http) {
-	var manager = {};
-
-	var played = true;
-	var paused = false;
-	var errPlay = false;
-	var errPause = false;
-	
-
-	manager.SetPlay = function () {
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Input.ExecuteAction", "params":{"action":"play"}}&callback=JSON_CALLBACK';
-
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				paused = false;
-				played= true;
-				window.location = "#/settings";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-				errPlay = true;
-			});
-	};
-	
-	manager.SetPause = function () {
-		ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Input.ExecuteAction", "params":{"action":"pause"}}&callback=JSON_CALLBACK';
-
-		$http.jsonp(window.base_url+ping_url)
-			.success(function(data, status){
-				played=false;
-				paused=true;
-				window.location = "#/settings";
-				window.location = "#/remote";
-			})
-			.error(function(data, status){
-				errPause = true;
-			});
-	};	
-	
-	
-		manager.getPaused = function () {
-			return paused;
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					volume = sound;
+					window.location = "#/settings";
+					window.location = "#/remote";				
+				})
+				.error(function(data, status){
+				});	
+		
+		}
+		
+		sounder.getMuted = function () {
+			return muted;
 		};
-	
-		manager.getPlayed = function () {
-			return played;
+		
+		sounder.getVolume = function () {
+				return volume;
 		};
 
 
 
-	return manager;
-});
+		return sounder;
+	});
+
+	app.factory('Manager', function($http) {
+		var manager = {};
+
+		var played = true;
+		var paused = false;
+		var errPlay = false;
+		var errPause = false;
+		
+
+		manager.SetPlay = function () {
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Input.ExecuteAction", "params":{"action":"play"}}&callback=JSON_CALLBACK';
+
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					paused = false;
+					played= true;
+					window.location = "#/settings";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+					errPlay = true;
+				});
+		};
+		
+		manager.SetPause = function () {
+			ping_url = '/jsonrpc?request={"jsonrpc":"2.0","method":"Input.ExecuteAction", "params":{"action":"pause"}}&callback=JSON_CALLBACK';
+
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					played=false;
+					paused=true;
+					window.location = "#/settings";
+					window.location = "#/remote";
+				})
+				.error(function(data, status){
+					errPause = true;
+				});
+		};	
+		
+		
+			manager.getPaused = function () {
+				return paused;
+			};
+		
+			manager.getPlayed = function () {
+				return played;
+			};
+
+
+
+		return manager;
+	});
+
+
+
+
+	app.factory('Runtime', function($http) {
+		var runtime = {};
+
+		var moment;
+		var moment2;
+
+
+		
+		
+			
+		
+
+		
+		
+		runtime.SetRuntime = function (moment) {
+		
+				
+			ping_url = '/jsonrpc?request={ "jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1 }&callback=JSON_CALLBACK';
+			var ping_url2;
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					ping_url2 = '/jsonrpc?request={"jsonrpc":"2.0","id":1,"method":"Player.Seek","params":{"playerid":'+data.result[0].playerid+',"value":' + moment+'}}&callback=JSON_CALLBACK';
+	$http.jsonp(window.base_url+ping_url2)
+					.success(function(data, status){
+								
+					})
+					.error(function(data, status){
+				
+					});
+								
+				})
+				.error(function(data, status){
+				
+				});	
+
+			
+
+		
+		};
+		
+		
+		
+		runtime.GetRuntime = function () {
+				
+				
+			ping_url = '/jsonrpc?request={ "jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1 }&callback=JSON_CALLBACK';
+			var ping_url2;
+			$http.jsonp(window.base_url+ping_url)
+				.success(function(data, status){
+					ping_url2 = '/jsonrpc?request={"jsonrpc":"2.0","id":1,"method":"Player.GetProperties","params":{"playerid":'+data.result[0].playerid+',"properties":["percentage", "time" ] }}&callback=JSON_CALLBACK';
+	$http.jsonp(window.base_url+ping_url2)
+					.success(function(data, status){
+						
+						moment2=data.result.percentage;
+					
+						console.log("estce bon" + moment2);
+									
+					})
+					.error(function(data, status){
+				
+					});
+								
+				})
+				.error(function(data, status){
+				});
+	return moment2;	
+		};
+
+
+
+		return runtime;
+	});
